23a4b5097172cedfa97ae83581b70da8799a96ab
Rajesh Patodia <rajesh.patodia@anchanto.com>
2021-02-05 21:20:31 +0530

    Implement create,edit and update articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,42 @@
 class ArticlesController < ApplicationController
+
   def show
     @article = Article.find(params[:id])
   end
+
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new 
+  end
+
+  def edit 
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    #render plain: params[:article]  
+    @article = Article.new(params.require(:article).permit(:title,:description,:author))
+    if @article.save
+      flash[:notice] = "Article was successfully created."
+      redirect_to @article
+    
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title,:description,:author))
+      flash[:notice] = "Article was Updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end
+
+  end
+
 end
