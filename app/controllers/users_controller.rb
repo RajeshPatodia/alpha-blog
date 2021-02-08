686e5646ba0991586dd57f06f0e5e4ebcb8c8c36
Rajesh Patodia <rajesh.patodia@anchanto.com>
2021-02-08 22:53:18 +0530

    Add edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(whitelist)
+      flash[:notice] = "Profile was Updated successfully."
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
   def whitelist
     params.require(:user).permit(:username, :email, :password)
   end
