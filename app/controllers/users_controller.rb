77c7c9b68301dfe0833ecfbdbc29abf473937899
Rajesh Patodia <rajesh.patodia@anchanto.com>
2021-02-09 19:07:50 +0530

    Add sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def new
     @user = User.new
   end
@@ -6,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(whitelist)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to Alpha Blog #{@user.username}, you have successfully signed up!."
       redirect_to articles_path
     
@@ -14,15 +17,12 @@ class UsersController < ApplicationController
     end
   end
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(whitelist)
       flash[:notice] = "Profile was Updated successfully."
       redirect_to @user
@@ -39,5 +39,9 @@ class UsersController < ApplicationController
 
   def whitelist
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
